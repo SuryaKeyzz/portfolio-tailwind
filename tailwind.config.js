8eecc9e25b7a594ef45fa95c571e51a3ba77db29
SuryaKeyzz <gilbertsuryali1@gmail.com>
2024-07-25 04:07:31 +0800

    update portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#2C7873",
+        dark: "#0f172a",
+        tosca: "#5b247a",
+        blues: "#1bcedf",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
