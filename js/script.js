7427de3ee455496f4aedec8523b9e7c5db9d9b70
SuryaKeyzz <gilbertsuryali1@gmail.com>
2024-07-25 20:26:44 +0800

    membuat perbaikan fitur + update dark mode

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
 // navbar fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTOp = document.querySelector("#to-top");
 
-    if(window.scrollY > fixedNav ){
-        header.classList.add('navbar-fixed');
-    }
-    else{
-        header.classList.remove('navbar-fixed');
-    }
-}
-
-
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTOp.classList.remove("hidden");
+    toTOp.classList.add("flex");
+  } else {
+    header.classList.add("flex");
+    toTOp.classList.add("hidden");
+    toTOp.classList.remove("flex");
+  }
+};
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click',function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
-})
+// klik diluar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
 
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
